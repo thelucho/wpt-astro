90507069722ee0c222cbf687dfbae7a2e230648e
Luciano Dichiara <luciano@studiohakuna.com>
2023-09-15 14:25:07 -0300

    Initial setup

---
+++ src/data/menu.js
@@ -0,0 +1,24 @@
+const menuItems = [
+  {
+    name: 'Home',
+    slug: '/'
+  },
+  {
+    name: 'Tickets',
+    slug: '/tickets'
+  },
+  {
+    name: 'Store',
+    slug: '/store'
+  },
+  {
+    name: 'Ranking',
+    slug: '/ranking'
+  },
+  {
+    name: 'Contact',
+    slug: '/contact'
+  },
+]
+
+export default menuItems
